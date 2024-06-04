dbfccaa2e0a8491fb3b640bbcfeaed0eb86f1d21
vishal kanawade <vishalkanawade2051@gmail.com>
2024-06-04 13:35:32 +0530

    all-in one

---
+++ file/FileHandling.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace file
+{
+    public class FileHandling
+    {
+        public void WriteUsingAppenAllText(string path)
+        {
+            Console.WriteLine("Enter author name");
+            var author = Console.ReadLine();
+            StringBuilder sb = new();
+            sb.Append(author).AppendLine(" My Foot ");
+            File.AppendAllText(path, sb.ToString());
+        }
+
+        public void WriteUsingFileStream(string path)
+        {
+            using (FileStream fileStream = new(path, FileMode.Append, FileAccess.Write))
+            {
+                Console.WriteLine("Enter author name");
+                var author = Console.ReadLine();
+                byte[] bytes = Encoding.UTF8.GetBytes(author + Environment.NewLine);
+                fileStream.Write(bytes, 0, bytes.Length);
+            }
+
+        }
+
+        public void WriteUsingStreamWriter(string path)
+        {
+            Console.WriteLine("Enter author name");
+            var author = Console.ReadLine();
+            using (StreamWriter sw = new(path, true))
+            {
+                sw.WriteLine(author);
+            }
+
+
+        }
+        public void ReadUsingStreamReader(string path)
+        {
+            using (StreamReader sr = new(path))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            };
+
+        }
+
+
+        public void ReadUsingFileStream(string path)
+        {
+            using (FileStream fs = new(path, FileMode.Open, FileAccess.Read))
+            {
+
+                Byte[] bytesBuffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = fs.Read(bytesBuffer, 0, bytesBuffer.Length)) > 0)
+                {
+                    string text = Encoding.UTF8.GetString(bytesBuffer);
+                    Console.WriteLine(text);
+                }
+            };
+
+        }
+
+        public void DeleteFileUsingFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+                Console.WriteLine("File Deleted");
+            }
+            else
+            {
+                Console.WriteLine("File not found");
+            }
+        }
+
+        public void DeleteFileUsingFileInfo(string path)
+        {
+            FileInfo fileInfo = new FileInfo(path);
+            if (fileInfo.Exists)
+            {
+                File.Delete(path);
+                Console.WriteLine("File Deleted");
+            }
+            else
+            {
+                Console.WriteLine("File not found");
+            }
+
+        }
+    }
+}
--- file/Program.cs
+++ file/Program.cs
@@ -1,33 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using file;
 using System.Text;
 
-Console.WriteLine("Hello, World!");
 
 string path = @"D:\Helper\author.txt";
-FileInfo fileInfo = new FileInfo(path);
-Console.WriteLine("Enter Author Id");
-var authorId = Console.ReadLine();
-Console.WriteLine("Enter Author Name");
-var authorName = Console.ReadLine();
-using (FileStream fileStream = fileInfo.Open(FileMode.Append, FileAccess.Write))
-{
-    
-    byte[] bytes = Encoding.UTF8.GetBytes(authorId + authorName + Environment.NewLine);
 
-    fileStream.Write(bytes, 0, bytes.Length);
-}
+FileHandling handler = new();
+//Console.WriteLine("Writing to the file");
+//handler.WriteUsingFileStream(path);
+//handler.WriteUsingAppenAllText(path);
+//handler.WriteUsingStreamWriter(path);
 
-StringBuilder sb = new StringBuilder();
-sb.Append(authorId).Append("  ").Append(authorName).AppendLine("     xsddw ");
-string x = sb.ToString();
-File.AppendAllText(path, x);
-string y = File.ReadAllText(path);
-Console.WriteLine(y);
+Console.WriteLine("Reading From the file");
+handler.ReadUsingStreamReader(path);
+handler.ReadUsingFileStream(path);
 
-
-
-
-Type t = typeof(float); ;
-Console.WriteLine(t.Module);
-Console.WriteLine(t.BaseType);
-Console.WriteLine(t.Namespace);
+Console.WriteLine("deleting the file");
+handler.DeleteFileUsingFile(path);
+handler.DeleteFileUsingFileInfo(path);
