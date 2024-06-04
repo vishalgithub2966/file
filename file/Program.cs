00cab0d5029a25f554f642fc5b2f1ccf75947891
vishal kanawade <vishalkanawade2051@gmail.com>
2024-06-04 08:03:05 +0530

    finalm touch

--- file/Program.cs
+++ file/Program.cs
@@ -3,32 +3,31 @@ using System.Text;
 
 Console.WriteLine("Hello, World!");
 
-string path = @"C:\Users\LNV-194\Documents\author.txt";
-//FileInfo fileInfo= new FileInfo(path);
-//FileStream fileStream = fileInfo.Open(FileMode.Open);
-//Console.WriteLine("Enter Author Id");
-//var authorId = Console.ReadLine();
-//Console.WriteLine("Enter Author Name");
-//var authorName = Console.ReadLine();
+string path = @"D:\Helper\author.txt";
+FileInfo fileInfo = new FileInfo(path);
+Console.WriteLine("Enter Author Id");
+var authorId = Console.ReadLine();
+Console.WriteLine("Enter Author Name");
+var authorName = Console.ReadLine();
+using (FileStream fileStream = fileInfo.Open(FileMode.Append, FileAccess.Write))
+{
+    
+    byte[] bytes = Encoding.UTF8.GetBytes(authorId + authorName + Environment.NewLine);
 
-//StringBuilder sb=new StringBuilder();
-//sb.Append(authorId).Append("  ").Append(authorName).AppendLine("     xsddw ");
-//string x=sb.ToString();
-//File.AppendAllText(path, x);
-//string y=File.ReadAllText(path);
-//Console.WriteLine(y);
+    fileStream.Write(bytes, 0, bytes.Length);
+}
+
+StringBuilder sb = new StringBuilder();
+sb.Append(authorId).Append("  ").Append(authorName).AppendLine("     xsddw ");
+string x = sb.ToString();
+File.AppendAllText(path, x);
+string y = File.ReadAllText(path);
+Console.WriteLine(y);
 
 
-//StreamWriter sw = new StreamWriter(fileStream);
-//sw.Write(authorId);
-//sw.Write(authorName);
-//sw.Close();
-//StreamReader sr = new StreamReader(fileStream);   
-//string str=sr.ReadLine();
-//Console.WriteLine($" {str}");
-//sr.Close();
 
-Type t = typeof(float);
+
+Type t = typeof(float); ;
 Console.WriteLine(t.Module);
 Console.WriteLine(t.BaseType);
 Console.WriteLine(t.Namespace);
